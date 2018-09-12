7696a9043e659fa0ea948ce67c64a825dd8d393a
Ryan <ryan.d.elliott@gmail.com>
2018-09-11 22:40:43 -0600

    render pie chart

--- simple-pie-chart-vue/src/components/PieChart.vue
+++ simple-pie-chart-vue/src/components/PieChart.vue
@@ -1,33 +1,22 @@
 <template>
   <svg :height="chartHeight" :width="chartWidth">
     <g :transform="chartTransform">
-      <g v-for="(data, i) in chartData" :key="i" class="arc">
-        <path :d="pathData" :fill="chartColors[i]"></path>
+      <g v-for="(data, i) in pieData" :key="i" class="arc">
+        <path :d="getPath(data)" :fill="chartColors[i]"></path>
         <!-- add  and :dy="" -->
       </g>
     </g>
   </svg>
 </template>
 
-<!-- things to maybe add in again -->
-<!--  -->
-<!-- was on svg maybe? :radius="chartRadius" -->
-<!-- <pie :sort="null" :value="data.population"> -->
-<!-- </pie> -->
-<!-- TODO: add this back in on text element>> :transform="textTransform"  -->
-<!-- "translate(" + label.centroid(d) + ")" -->
-<!-- <text fill="black">
-          {{ data.age }}
-        </text> -->
-
 <script lang="ts">
   import Vue from 'vue';
-  // import { d3, arc } from 'd3-path';
+  import { pie, arc, PieArcDatum, DefaultArcObject } from 'd3-shape';
 
   export default Vue.extend({
     props: {
       chartData: {
-        type: Array as () => { value:number }[],
+        type: Array as () => { population:number, age:string }[],
         required: true,
       },
     },
@@ -42,13 +31,13 @@
         largeArcFlag: 100
       };
     },
-    // methods: {
-    //   pathData() {
-    //     return d3.arc()
-    //       .outerRadius(this.endX - 10)
-    //       .innerRadius(0);
-    //   }
-    // },
+    methods: {
+      getPath(arcData: DefaultArcObject): string|null {
+        const arcGenerator = arc().outerRadius(this.chartRadius - 10).innerRadius(0);
+        const path = arcGenerator(arcData);
+        return path;
+      },
+    },
     computed: {
       chartRadius(): number {
         return Math.min(this.chartWidth, this.chartHeight) / 2;
@@ -59,12 +48,10 @@
       endX(): number {
         return this.chartRadius - 10;
       },
-      pathData(): string {
-        // function(x, y, r, a0, a1, ccw)
-        // M1.469576158976824e-14,-240A240,240,0,0,1,107.0492887034484,-214.803281604555L0,0Z
-        //return `M${this.startX} ${this.startY},${this.endX},0,0,1,${this.largeArcFlag},${this.endY},0Z`;
-        return `M${this.startX} ${this.startY}, A 1 1 0 ${this.largeArcFlag} 1 ${this.endX} ${this.endY}, L 0 0`;
-      }
+      pieData(): PieArcDatum<number | { valueOf(): number; }>[] {
+        const data: number[] = this.chartData.map(d => d.population);
+        return pie()(data);
+      },
     }
   });
 </script>
