0f2243eaa1abcc4737f4f4be14ed9c0019c159f4
Ryan <ryan.d.elliott@gmail.com>
2018-09-13 22:34:02 -0600

    code clean up and documentation

--- simple-pie-chart-vue/src/components/PieChart.vue
+++ simple-pie-chart-vue/src/components/PieChart.vue
@@ -1,9 +1,15 @@
 <template>
   <svg :height="chartHeight" :width="chartWidth">
+    <!-- Pie Chart Group -->
     <g :transform="chartTransform">
-      <g v-for="(data, i) in pieData" :key="i" class="arc">
-        <path :d="getPath(data)" :fill="chartColors[i]"></path>
-        <text :transform="getLabelTransform(data)" dy="0.35em">{{ chartData[i].age }}</text>
+      <!-- Slice Groups -->
+      <g v-for="(data, i) in chartData" :key="i" class="arc">
+        <!-- Slice Arc -->
+        <path :d="arcPaths[i]" :fill="chartColors[i]"></path>
+        <!-- Slice Label -->
+        <text :transform="labelTransforms[i]" dy="0.35em">
+          {{ data.age }}
+        </text>
       </g>
     </g>
   </svg>
@@ -11,7 +17,7 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import { pie, arc, PieArcDatum, DefaultArcObject } from 'd3-shape';
+  import { pie as d3Pie, arc as d3Arc, PieArcDatum, DefaultArcObject, Arc } from 'd3-shape';
 
   export default Vue.extend({
     props: {
@@ -27,28 +33,43 @@
         chartColors: ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"],
       };
     },
-    methods: {
-      getPath(arcData: DefaultArcObject): string|null {
-        const arcGenerator = arc().outerRadius(this.chartRadius - 10).innerRadius(0);
-        const path = arcGenerator(arcData);
-        return path;
-      },
-      getLabelTransform(data: DefaultArcObject): string {
-        const arcGenerator = arc().outerRadius(this.chartRadius - 40).innerRadius(this.chartRadius - 40);
-        const arcCenterCoords = arcGenerator.centroid(data);
-        return `translate(${arcCenterCoords})`;
-      },
-    },
     computed: {
-      chartRadius(): number {
-        return Math.min(this.chartWidth, this.chartHeight) / 2;
-      },
+      /** Transform attr for centering the chart */
       chartTransform(): string {
         return "translate(" + this.chartWidth / 2 + "," + this.chartHeight / 2 + ")";
       },
-      pieData(): PieArcDatum<number | { valueOf(): number; }>[] {
-        const data: number[] = this.chartData.map(d => d.population);
-        return pie()(data);
+      /** The max radius of the pie chart depending on the chart's dimensions */
+      chartRadius(): number {
+        return Math.min(this.chartWidth, this.chartHeight) / 2;
+      },
+      /** An initialized d3 Arc Generator function for the pie slices */
+      pieArcGenerator(): Arc<any, DefaultArcObject> {
+        const arcGen = d3Arc().outerRadius(this.chartRadius - 10).innerRadius(0);
+        return arcGen;
+      },
+      /** An initialized d3 Arc Generator function for the labels */
+      labelArcGenerator(): Arc<any, DefaultArcObject> {
+        const arcGen = d3Arc().outerRadius(this.chartRadius - 40).innerRadius(this.chartRadius - 40);
+        return arcGen;
+      },
+      /** Arc objects used by arg generator to generate path info */
+      pieArcObjects(): PieArcDatum<number | { valueOf(): number; }>[] {
+        const populationData: number[] = this.chartData.map(d => d.population);
+
+        // Use d3 pie generator to get data objects used by arc generator
+        return d3Pie()(populationData);
+      },
+      /** Path inputs for the path tag to draw the pie slices */
+      arcPaths(): string[] {
+        // Use arc generator to calculate slice shapes for <path> tag
+        const arcs = this.pieArcObjects.map((arcDatum): string => this.pieArcGenerator(arcDatum));
+        return arcs;
+      },
+      /** Transforms for positioning pie slice labels */
+      labelTransforms(): string[] {
+        const sliceCenterCoords = this.pieArcObjects.map((arcDatum) => this.labelArcGenerator.centroid(arcDatum));
+        const transforms = sliceCenterCoords.map(center => `translate(${center})`)
+        return transforms;
       },
     }
   });
